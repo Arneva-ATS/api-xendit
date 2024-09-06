45fcc351e557265e284d90f22bf135b785de52a4
aditbest5 <aditbest5@gmail.com>
2024-09-06 14:53:56 +0700

    tes

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,13 @@ import helmet from 'helmet';
 
 const app = express();
 // Konfigurasi CORS yang lebih spesifik
-const corsOptions = {
-	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-	allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
-	credentials: true, // Jika Anda menggunakan kredensial seperti cookies, pastikan ini diatur
-  };
+// const corsOptions = {
+// 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+// 	allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+// 	credentials: true, // Jika Anda menggunakan kredensial seperti cookies, pastikan ini diatur
+//   };
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet());
 app.use('/v1/api', apiRoutes);
 app.use(errorHandler);
