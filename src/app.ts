097fc3f068bf6779c31c5372db91cea96e66d7b5
aditbest5 <aditbest5@gmail.com>
2024-09-06 14:34:25 +0700

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,15 @@ import cors from 'cors';
 import helmet from 'helmet';
 
 const app = express();
-
+// Konfigurasi CORS yang lebih spesifik
+const corsOptions = {
+	origin: 'https://rkiapp.arnevats.com', // Ganti ini dengan domain yang Anda izinkan
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+	allowedHeaders: ['Content-Type', 'Authorization'],
+	credentials: true, // Jika Anda menggunakan kredensial seperti cookies, pastikan ini diatur
+  };
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use('/v1/api', apiRoutes);
 app.use(errorHandler);
